c89211cd441d6e0881f8c9ad0bf7cbb04d5861b2
KaioAlexandredeMorais <kaio.morais@iesb.edu.br.com>
2026-03-27 21:13:54 -0300

    Atividade feita

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -1,14 +1,61 @@
-// src/App.tsx
-import { Heading } from './components/Heading';
-import { Timer } from 'lucide-react'; // Importando o ícone
+import './styles/theme.css';
+import './styles/global.css';
+
+import { Container } from './components/Container';
+import { Menu } from './components/Menu';
+import { Logo } from './components/Logo';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
+import { DefaultButton } from './components/DefaultButton';
+import { Cycles } from './components/Cycles';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
-    <Heading>
-      Histórico
-      <button>
-        <Timer /> {/* Renderizando o ícone dentro do botão */}
-      </button>
-    </Heading>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles />
+          </div>
+
+          {/* Testando os nossos botões dinâmicos */}
+          <div className='formRow'>
+            {/* Como o color padrão é 'green', não precisamos passar a prop aqui! */}
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
+
+            {/* Forçando o botão a ser vermelho e mudando o ícone */}
+            <DefaultButton icon={<StopCircleIcon />} color='red' />
+          </div>
+        </form>
+      </Container>
+      <Container>
+        <Footer />
+      </Container>
+    </>
   );
 }
--- chronos-pomodoro/src/components/Heading.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'; // Importante importar o React
-
-type HeadingProps = {
-  // Agora aceitamos texto OU outros componentes JSX!
-  children: React.ReactNode;
-};
