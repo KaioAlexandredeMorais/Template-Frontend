66c17d37a2988f78ea8bc1a778c518d7da3781e1
KaioAlexandredeMorais <kaio.morais@iesb.edu.br.com>
2026-04-09 21:25:20 -0300

    terminar depois, parei na 37

--- chronos-pomodoro/src/components/MainForm/index.tsx
+++ chronos-pomodoro/src/components/MainForm/index.tsx
@@ -2,10 +2,26 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
+import { useTaskContext } from '../contexts/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+
+  // 1. Criamos a função Handler para o formulário
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    // 2. Previne o comportamento padrão (recarregar a página)
+    event.preventDefault();
+
+    // 3. Log de teste para confirmar que funcionou
+    console.log('DEU CERTO');
+  }
+
+  // Apenas removi a função handleClick do botão de teste da aula passada,
+  // pois já limpamos isso no passo anterior.
+
   return (
-    <form className='form' action=''>
+    // 4. Conectamos a nossa função ao evento onSubmit do form
+    <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='task'
@@ -16,7 +32,7 @@ export function MainForm() {
       </div>
 
       <div className='formRow'>
-        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Próximo intervalo é de 25min</p>
       </div>
 
       <div className='formRow'>
