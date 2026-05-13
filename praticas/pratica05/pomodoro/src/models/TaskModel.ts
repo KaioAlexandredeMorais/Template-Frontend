34a776e3cf8c4abf20910d830f7ddce9d303a955
KaioAlexandredeMorais <kaio.morais@iesb.edu.br.com>
2026-05-13 19:44:43 -0300

    Remove projeto bagunçado

--- praticas/pratica05/pomodoro/src/models/TaskModel.ts
+++
@@ -1,9 +0,0 @@
-export interface TaskModel {
-  id: string;
-  name: string;
-  startDate: Date;
-  completeDate: Date | null; // Verifique se o nome aqui é igual ao que você usa no MainForm
-  interruptDate: Date | null;
-  duration: number;
-  type: string;
-}
